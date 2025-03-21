342bff9f1045b411613ba954c58bf39f3865cb6e
Jonathan Hooper <jonathan.hooper@opensocietyfoundations.org>
2025-03-21 10:54:55 -0400

    Use enviroment variables for password protection

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,6 @@
 export const onRequest = async ({ request, env }) => {
     const auth = request.headers.get("Authorization");
-    const expectedAuth = "Basic " + btoa("nycfixit:repair");
+    const expectedAuth = "Basic " + btoa(`${env.HTTP_AUTH_USERNAME}:${env.HTTP_AUTH_PASSWORD}`);
     if (auth !== expectedAuth) {
         return new Response("Unauthorized", { status: 401, headers: { "WWW-Authenticate": "Basic" } });
     }
